9b94b5e71d5239ae74ccfba196a7ab255b6bd1ed
Brody Hughes <bodyhughes@gmail.com>
2022-05-13 16:52:00 -0700

    Added premake5 file

--- premake5.lua
+++ premake5.lua
@@ -7,28 +7,20 @@ project "ImGui"
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
+        "*.h",
+        "*.cpp"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
+		staticruntime "Off"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
+		staticruntime "Off"
 
 	filter "configurations:Debug"
 		runtime "Debug"
